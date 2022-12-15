722feadf37cb8bc3691918447bead30eeae39e55
Aditya Kumar Pal <119075685+AdityaKumarPal@users.noreply.github.com>
2022-12-16 02:18:44 +0530

    Update models.py

--- bank/models.py
+++ bank/models.py
@@ -2,7 +2,7 @@ import mysql.connector
 from bank import bcrypt, login_manager
 
 class User:
-    mydb = mysql.connector.connect(host="localhost", user="root", passwd="Aditya@997", database="abi_project")
+    mydb = mysql.connector.connect(host="localhost", user="root", passwd="MySQL_Password", database="Database_Name")
     cursor = mydb.cursor()
     cursor.execute("select * from users")
     cursor.fetchall()
